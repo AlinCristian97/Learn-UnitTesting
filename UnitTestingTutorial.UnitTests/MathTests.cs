3935f883514ff0e0ee5e246e7e9faa35f4360d3d
Alin Cristian <alin.cristian977@gmail.com>
2021-06-29 15:02:19 +0200

    Initial commit

---
+++ UnitTestingTutorial.UnitTests/MathTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace UnitTestingTutorial.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+            
+            int result = math.Add(1, 2);
+            
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
