f98d2aad0a39a47461ca4cd0b5b023f453f2961c
Alin Cristian <alin.cristian977@gmail.com>
2021-07-01 00:12:37 +0200

    Refactored towards a Loosely-coupled Design

---
+++ UnitTestingTutorial.UnitTests/FakeFileReader.cs
@@ -0,0 +1,12 @@
+﻿using UnitTestingTutorial.Mocking;
+
+namespace UnitTestingTutorial.UnitTests
+{
+    public class FakeFileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return "";
+        }
+    }
+}
