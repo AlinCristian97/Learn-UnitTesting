61a1ee3517c34bf1795c4ff144a216b5526ed208
Alin Cristian <alin.cristian977@gmail.com>
2021-06-30 13:23:45 +0200

    Exercise - FizzBuzz

---
+++ UnitTestingTutorial.UnitTests/FizzBussTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using UnitTestingTutorial.Fundamentals;
+
+namespace UnitTestingTutorial.UnitTests
+{
+    [TestFixture]
+    public class FizzBussTests
+    {
+        [Test]
+        public void GetOutput_InputDivisibleBy3And5_ReturnFizzBuzz()
+        {
+            string result = FizzBuzz.GetOutput(15);
+            
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+        
+        [Test]
+        public void GetOutput_InputDivisibleBy3Only_ReturnFizz()
+        {
+            string result = FizzBuzz.GetOutput(3);
+            
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        
+        [Test]
+        public void GetOutput_InputDivisibleBy5Only_ReturnBuzz()
+        {
+            string result = FizzBuzz.GetOutput(5);
+            
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputNotDivisibleBy3Or5_ReturnInput()
+        {
+            string result = FizzBuzz.GetOutput(4);
+            
+            Assert.That(result, Is.EqualTo(4.ToString()));
+        }
+        
+        
+    }
+}
