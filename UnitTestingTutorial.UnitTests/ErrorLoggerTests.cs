2f6eb52c91b22b3e632ca0359704ef15577226a1
Alin Cristian <alin.cristian977@gmail.com>
2021-06-30 10:00:13 +0200

    Testing Methods that Throw Exceptions

--- UnitTestingTutorial.UnitTests/ErrorLoggerTests.cs
+++ UnitTestingTutorial.UnitTests/ErrorLoggerTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace UnitTestingTutorial.UnitTests
@@ -14,6 +15,26 @@ namespace UnitTestingTutorial.UnitTests
             logger.Log("a");
             
             Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            //TODO: Move logger initialization in the NUnit SetUp method
+            var logger = new ErrorLogger();
+            
+            // Instead of this:
+            // logger.Log(error);
+            
+            // We use a delegate / lambda expression in the Assertion
+            // Shorter syntax:
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+            
+            // Longer syntax:
+            // Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>());
         }
     }
 }
