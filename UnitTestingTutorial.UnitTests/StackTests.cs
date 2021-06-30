07040f6a6e688a1d0c16553cdbeafccced93c993
Alin Cristian <alin.cristian977@gmail.com>
2021-06-30 21:55:06 +0200

    Exercise - Stack

---
+++ UnitTestingTutorial.UnitTests/StackTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using NUnit.Framework;
+using UnitTestingTutorial.Fundamentals;
+
+namespace UnitTestingTutorial.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        Stack<object> _stack;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _stack = new Stack<object>();
+        }
+        
+        [Test]
+        public void Count_EmptyStack_Return0()
+        {
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+        
+        [Test]
+        public void Push_NullArgument_ThrowArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArgument_AddArgumentInStack()
+        {
+            object obj = new object();
+            _stack.Push(obj);
+            
+            Assert.That(_stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.Exception.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Pop_NotEmptyStack_RemoveLastElementInStack()
+        {
+            object obj = new object();
+            _stack.Push(obj);
+            _stack.Push(obj);
+            _stack.Push(obj);
+            _stack.Pop();
+            
+            Assert.That(_stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Pop_WhenCalled_ReturnRemovedElement()
+        {
+            object obj = new object();
+            object obj1 = new object();
+            object obj2 = new object();
+            
+            _stack.Push(obj);
+            _stack.Push(obj1);
+            _stack.Push(obj2);
+            
+            object removedElement = _stack.Pop();
+            
+            Assert.That(removedElement, Is.EqualTo(obj2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.Exception.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Peek_NotEmptyStack_ReturnLastElementInStack()
+        {
+            object obj = new object();
+            _stack.Push(obj);
+
+            object lastElementInStack = _stack.Peek();
+            
+            Assert.That(lastElementInStack, Is.EqualTo(obj));
+        }
+
+        [Test]
+        public void Peek_NotEmptyStack_DoesNotRemoveLastElementInStack()
+        {
+            object obj = new object();
+            object obj1 = new object();
+            object obj2 = new object();
+            
+            _stack.Push(obj);
+            _stack.Push(obj1);
+            _stack.Push(obj2);
+
+            _stack.Peek();
+            
+            Assert.That(_stack.Count, Is.EqualTo(3));
+        }
+    }
+}
---
+++ h
@@ -0,0 +1,87 @@
+[33mcommit 27fb0e64fe0a6a0044d6a580aff16a49fbac8c26[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 18:02:55 2021 +0200
+
+    Exercise - DemeritPointsCalculator
+
+[33mcommit 61a1ee3517c34bf1795c4ff144a216b5526ed208[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 13:23:45 2021 +0200
+
+    Exercise - FizzBuzz
+
+[33mcommit f5e47c77bd67f9589823a92e27232b6e9dfe2eda[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 10:55:19 2021 +0200
+
+    Testing Private Methods
+    
+    Updated Namespaces
+
+[33mcommit c1777227e8411b69e4f65ccc983d2dbdf889a01a[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 10:06:45 2021 +0200
+
+    Testing Methods that Raise an Event
+
+[33mcommit 2f6eb52c91b22b3e632ca0359704ef15577226a1[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 10:00:13 2021 +0200
+
+    Testing Methods that Throw Exceptions
+
+[33mcommit e3dd4062efe4f476d23dec11d0911dd20507bc57[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 09:49:31 2021 +0200
+
+    Testing Void Methods
+
+[33mcommit 2f3961f84cd72bd8c540f4064cdbba58bce122b1[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 09:43:22 2021 +0200
+
+    Testing the Return Type of Methods
+
+[33mcommit cbadb41b25ca8731aa63945c80dedf37dfb129cd[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Wed Jun 30 00:12:02 2021 +0200
+
+    Testing Arrays and Collections
+
+[33mcommit 4b7f4c00dc76445f79c7f4b743b372a082fb5644[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Tue Jun 29 23:37:04 2021 +0200
+
+    Testing Strings
+
+[33mcommit b0394baada67514d1c37d340e00836889a771723[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Tue Jun 29 22:26:37 2021 +0200
+
+    Tested 'Ignore' attribute
+
+[33mcommit 143cdebfd5005ae49c9794c1b4f616aa8e89d9aa[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Tue Jun 29 22:23:24 2021 +0200
+
+    Simplified Max Test method using Parameterized Tests
+
+[33mcommit 033ee202dfea78831690b013c4aaebcf77c8b07e[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Tue Jun 29 16:26:19 2021 +0200
+
+    Added 'SetUp' method in the 'MathTests' class
+    
+    This method runs at the beginning of each test
+
+[33mcommit 36dceb4fb00f646ac4120ddb658b3814bc95f7e7[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Tue Jun 29 15:33:34 2021 +0200
+
+    Added Unit Tests for 'Max' method in 'Math' class
+
+[33mcommit 3935f883514ff0e0ee5e246e7e9faa35f4360d3d[m
+Author: Alin Cristian <alin.cristian977@gmail.com>
+Date:   Tue Jun 29 15:02:19 2021 +0200
+
+    Initial commit
