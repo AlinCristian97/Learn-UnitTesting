63aa9e25d1784e6639096b3087cceaf3b60acd88
Alin Cristian <alin.cristian977@gmail.com>
2021-07-01 09:51:10 +0200

    Dependency Injection via Method Parameters

---
+++ UnitTestingTutorial.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UnitTestingTutorial.Mocking;
+
+namespace UnitTestingTutorial.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnErrorMessage()
+        {
+            var service = new VideoService();
+            
+            string result = service.ReadVideoTitle(new FakeFileReader());
+            
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
